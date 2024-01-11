d2da66be8e05b665fd622f65177ba86d26e02f8d
Jasper Burroughs <burrojas26@7hills.org>
2024-01-11 13:09:51 -0500

    Created some java files started working on the file io

---
+++ Course.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
+
+public class Course {
+    File myObj = new File("seminarKey.csv");
+    Scanner myScanner = new Scanner(myObj);
+}
